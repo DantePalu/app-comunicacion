6599b12806fb6489c086e16ef7aa11e5b5c63b3f
Dante Palumbo <dantepalumbo03@gmail.com>
2024-08-07 09:40:01 -0300

    appcomunicacio

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function mostrarAlerta() {
+    alert('Dirijase hacia la derecha')
+}
+function mostrarAlerta1() {
+    alert('Dirijase hacia la izquierda')
+}
+function mostrarAlerta2() {
+    alert('Dirijase hacia el ascensor')
+}
+
+
+
+document.getElementById('biblio').addEventListener('click', function(){ 
+var audio = document.getElementById('audio');
+audio.play()
+   })
+
+
