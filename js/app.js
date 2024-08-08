a44eddbd292dd70901d64adce50abd95dc4541e3
Dante Palumbo <dantepalumbo03@gmail.com>
2024-08-08 09:35:11 -0300

    segundo boceto

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
-function mostrarAlerta() {
-    alert('Dirijase hacia la derecha')
-}
-function mostrarAlerta1() {
-    alert('Dirijase hacia la izquierda')
-}
-function mostrarAlerta2() {
-    alert('Dirijase hacia el ascensor')
-}
 
+var sound = new Audio()
+sound.src = "./BIBLIO.mp3"
 
+var sound1 = new Audio()
+sound1.src = "./MEDICC.mp3"
 
-document.getElementById('biblio').addEventListener('click', function(){ 
-var audio = document.getElementById('audio');
-audio.play()
-   })
+var sound2 = new Audio()
+sound2.src = "./bano-accesible-audio.mp3"
 
+var sound3 = new Audio()
+sound3.src = "./podotactil.mp3"
 
+var sound4 = new Audio()
+sound4.src = "./PLATAFORMACC.mp3"
+
+var sound5 = new Audio()
+sound5.src = "./AROMG.mp3"
+
+var sound6 = new Audio()
+sound6.src = "./LACTARIO.mp3"
+
+var sound7 = new Audio()
+sound7.src = "./RAMPA.mp3"
+
+var sound8 = new Audio()
+sound8.src = "./PARK.mp3"
